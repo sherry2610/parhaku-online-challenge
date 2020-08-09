e12bf3ee3756df1da48dbdc6b61568bfa0867c8e
sherry2610 <shaharyarkhalid2610@gmail.com>
2020-08-09 06:31:54 +0500

    feat: add delete item from cart functionality

--- src/components/CartImageComp.js
+++ src/components/CartImageComp.js
@@ -1,12 +1,20 @@
 import React, { useContext } from "react";
-// import { PicSomeContext } from "../../context/PicSomeContext";
+import { PicSomeContext } from "../context/PicSomeContext";
+import DeleteIcon from '@material-ui/icons/Delete';
 import {Col,Image} from 'react-bootstrap'
 
-function CartImageComp({ image, serialNum }) {
+function CartImageComp({ image, serialNum, deleteItem }) {
+    // const {imgData,cartItems,setCartItems, setImgData} = useContext(PicSomeContext)
+    
+    
+ 
   return (
     <div className="cart-item-div">
         <span className="serial-num">{serialNum+1}</span>
         <img src={image.url} alt="cart-item" className='cart-item-img'/>
+        <span className="delete-icon">
+            <DeleteIcon onClick={()=>deleteItem(image.id)}/>
+        </span>
     </div>
   );
 }
--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,26 +1,46 @@
 import React, { useContext } from "react";
 import { PicSomeContext } from "../../context/PicSomeContext";
 import ImageComp from "../../components/ImageComp";
-import './index.css'
+import "./index.css";
 import { EmptyCart } from "../../components/EmptyCart";
 import CartImageComp from "../../components/CartImageComp";
 function Cart() {
-  const { cartItems, setCartItems } = useContext(PicSomeContext);
+  const { imgData, setImgData, cartItems, setCartItems } = useContext(
+    PicSomeContext
+  );
+  const deleteItem = (id) => {
+    const updatedImgData = imgData.map((img) => {
+      return img.id === id
+        ? {
+            ...img,
+            inCart: false,
+          }
+        : img;
+    });
+    const updatedCartData = cartItems.filter((item) => item.id !== id);
+    setCartItems(updatedCartData);
+    setImgData(updatedImgData);
+  };
+
   console.log("CARTITEMS", cartItems);
   if (cartItems.length > 0) {
     return (
       <div>
         <h1>CART</h1>
-        {cartItems.map((image,i) => (
-          <CartImageComp
-            image={image}
-            serialNum={i}
-            />
-        ))}
+        {cartItems.map(
+          (image, i) =>
+            image.inCart && (
+              <CartImageComp
+                image={image}
+                serialNum={i}
+                deleteItem={deleteItem}
+              />
+            )
+        )}
       </div>
     );
-  }else{
-   return <EmptyCart />
+  } else {
+    return <EmptyCart />;
   }
 }
 
